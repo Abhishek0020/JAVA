bf2913fcf974ac83a1815e0fa8eac8829b30374d
Abhishek0020 <90542072+Abhishek0020@users.noreply.github.com>
2023-07-01 23:22:44 +0530

    Add files via upload

---
+++ Array_demo.java
@@ -0,0 +1,20 @@
+//import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+//
+//import java.util.Scanner;
+//
+//public class Array_demo {
+//    public static void main(String[] args) {
+//        int size,i;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter the number of Array : ");
+//        size=sc.nextInt();
+//        int a[]=new int[size];
+//        for ( i = 0; i < size; i++) {
+//            a[i]=sc.nextInt();
+//        }
+//        System.out.println("print the Original array");
+//        for ( i = 0; i < size; i++) {
+//            System.out.println(""+a[i]);
+//        }
+//    }
+//}
